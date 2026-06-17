8ead435df33d1fe183e16728f82ea9c9c5ff0463
gaku <kd1374211@st.kobedenshi.ac.jp>
2026-06-17 11:12:24 +0900

    リザルトウィンドウ

---
+++ BaseFramework 2.5D/Src/Application/Object/Sprite/ResultWindow/ResultWindow.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+class ResultWindow :public KdGameObject
+{
+public:
+
+	ResultWindow() { Init(); }
+	~ResultWindow()override { Release(); }
+
+	void Update()override;
+	void DrawSprite()override;
+
+private:
+
+	void Init()override;
+	void Release();
+
+};
